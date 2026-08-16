6d2ba697cd236e4762c3d8747fccbc7b76d38115
MichaelDWL <michaeldouglas.devs@gmail.com>
2026-08-16 16:50:14 -0300

    feat: adicional modal de login

--- js/script.js
+++ js/script.js
@@ -2,14 +2,9 @@ import initScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
-import * as teste from "./modules/teste.js";
+import initModal from "./modules/modal.js";
 
-teste.teste1();
-teste.teste2();
-console.log(teste.senha);
-
-console.log(this);
-
+initModal();
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
