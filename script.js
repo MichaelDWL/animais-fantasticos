78360bdc445d861bb685df3db02a52ebe2c502f0
MichaelDWL <michaeldouglas.devs@gmail.com>
2026-08-16 15:54:07 -0300

    refactor: altera seletor para utilizar data no lugar de classe

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function initTabNav() {
-  const tabMenu = document.querySelectorAll(".js-tabmenu li");
-  const tabContent = document.querySelectorAll(".js-tabcontent section");
-  tabContent[0].classList.add("ativo");
+  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
+  const tabContent = document.querySelectorAll('[data-tab="content"] section');
 
   if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
+
     function activeTab(index) {
       tabContent.forEach((section) => {
         section.classList.remove("ativo");
       });
-
-      tabContent[index].classList.add("ativo");
+      const direcao = tabContent[index].dataset.anime;
+      tabContent[index].classList.add("ativo", direcao);
     }
 
     tabMenu.forEach((itemMenu, index) => {
@@ -21,13 +22,16 @@ function initTabNav() {
 }
 initTabNav();
 
-function initAccordionList() {
-  const accordionList = document.querySelectorAll(".js-accordion dt");
+function initAccordion() {
+  const accordionList = document.querySelectorAll(
+    '[data-anime="accordion"] dt',
+  );
   const activeClass = "ativo";
-  accordionList[0].classList.add(activeClass);
-  accordionList[0].nextElementSibling.classList.add(activeClass);
 
   if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+
     function activeAccordion() {
       this.classList.toggle(activeClass);
       this.nextElementSibling.classList.toggle(activeClass);
@@ -38,10 +42,12 @@ function initAccordionList() {
     });
   }
 }
-initAccordionList();
+initAccordion();
 
-function scrollSuave() {
-  const linksInternos = document.querySelectorAll(".js-menu a[href^='#']");
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll(
+    '[data-menu="suave"] a[href^="#"]',
+  );
 
   function scrollToSection(event) {
     event.preventDefault();
@@ -51,31 +57,38 @@ function scrollSuave() {
       behavior: "smooth",
       block: "start",
     });
+
+    // forma alternativa
+    // const topo = section.offsetTop;
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: 'smooth',
+    // });
   }
 
   linksInternos.forEach((link) => {
     link.addEventListener("click", scrollToSection);
   });
 }
-scrollSuave();
+initScrollSuave();
 
 function initAnimacaoScroll() {
-  const sections = document.querySelectorAll(".js-scroll");
+  const sections = document.querySelectorAll('[data-anime="scroll"]');
   if (sections.length) {
     const windowMetade = window.innerHeight * 0.6;
-    function animaScoll() {
+
+    function animaScroll() {
       sections.forEach((section) => {
         const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = (sectionTop - windowMetade < 0); // prettier-ignore
-        if (isSectionVisible) {
-          section.classList.add("ativo");
-        } else {
-          section.classList.remove("ativo");
-        }
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) section.classList.add("ativo");
+        else section.classList.remove("ativo");
       });
     }
-    animaScoll();
-    window.addEventListener("scroll", animaScoll);
+
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
   }
 }
 initAnimacaoScroll();
