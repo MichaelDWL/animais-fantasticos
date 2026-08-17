856b9e46b1ced64313d4ef079f9d66620415f753
MichaelDWL <michaeldouglas.devs@gmail.com>
2026-08-17 20:33:59 -0300

    feat: adiciona menu mobile

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,12 +1,8 @@
-import * as teste from "./teste.js";
-
 export default function initAccordion() {
-  const accordionList = document.querySelectorAll(
-    '[data-anime="accordion"] dt',
-  );
-  const activeClass = "ativo";
-
-  if (accordionList.length) {
+  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
+  const activeClass = 'ativo';
+  
+  if(accordionList.length) {
     accordionList[0].classList.add(activeClass);
     accordionList[0].nextElementSibling.classList.add(activeClass);
 
@@ -16,7 +12,7 @@ export default function initAccordion() {
     }
 
     accordionList.forEach((item) => {
-      item.addEventListener("click", activeAccordion);
+      item.addEventListener('click', activeAccordion);
     });
   }
 }
